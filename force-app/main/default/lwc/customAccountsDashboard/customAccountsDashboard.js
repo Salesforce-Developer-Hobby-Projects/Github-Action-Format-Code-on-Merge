8fbc713dbccb53e19566caa88ece941dd05d828f
ndrewr <ndrewr@users.noreply.github.com>
2025-01-17 07:32:51 +0000

    Prettified Code!

--- force-app/main/default/lwc/customAccountsDashboard/customAccountsDashboard.js
+++ force-app/main/default/lwc/customAccountsDashboard/customAccountsDashboard.js
@@ -1,22 +1,16 @@
 // AccountDashboard.js
 import { LightningElement, track, wire } from "lwc";
 
-import getAccounts from
- "@salesforce/apex/AccountController.getAllAccounts";
+import getAccounts from "@salesforce/apex/AccountController.getAllAccounts";
 
-import searchAccounts from 
-"@salesforce/apex/AccountController.getAccountsByName";
+import searchAccounts from "@salesforce/apex/AccountController.getAccountsByName";
 
-import createAccount from 
-"@salesforce/apex/AccountController.createAccount";
+import createAccount from "@salesforce/apex/AccountController.createAccount";
 
-import deleteAccount from
- "@salesforce/apex/AccountController.deleteAccount";
+import deleteAccount from "@salesforce/apex/AccountController.deleteAccount";
 
-export default 
-class AccountDashboard extends LightningElement 
-{
-    @track searchTerm = '';
+export default class AccountDashboard extends LightningElement {
+  @track searchTerm = "";
 
   @track newAccountName = "";
 
@@ -29,30 +23,22 @@ class AccountDashboard extends LightningElement
   loadAccounts() {
     getAccounts()
       .then((result) => {
-        this.accounts = result 
-        || 
-        [];
+        this.accounts = result || [];
       })
       .catch((error) => {
-        console.error(
-          error
-        );
+        console.error(error);
       });
   }
 
-  handleSearchChange(event) 
-  {
+  handleSearchChange(event) {
     this.searchTerm = event.target.value;
     this.searchAccounts();
   }
 
-  searchAccounts() 
-  {
+  searchAccounts() {
     searchAccounts({ name: this.searchTerm })
       .then((result) => {
-        this.accounts = result 
-        || 
-        [];
+        this.accounts = result || [];
       })
       .catch((error) => {
         console.error(error);
@@ -74,60 +60,54 @@ class AccountDashboard extends LightningElement
       });
   }
 
-    loadAccounts() 
-    {
-        getAccounts()
-            .then(
-                result => {
-                this.accounts = result || [];
-            }
-        )
-            .catch(error => {
-                console.error(error);
-            });
-    }
+  loadAccounts() {
+    getAccounts()
+      .then((result) => {
+        this.accounts = result || [];
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-    handleSearchChange( event ) 
-    {
-        this.searchTerm = event.target.value;
-        this.searchAccounts();
-    }
+  handleSearchChange(event) {
+    this.searchTerm = event.target.value;
+    this.searchAccounts();
+  }
 
-    searchAccounts() 
-    {
-        searchAccounts({ name: this.searchTerm })
-            .then(result => {
-                this.accounts = result || [];
-            })
-            .catch(
-                error => { console.error(error);
-    }   );
-    }
+  searchAccounts() {
+    searchAccounts({ name: this.searchTerm })
+      .then((result) => {
+        this.accounts = result || [];
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-    handleNameChange(event) {
-        this.newAccountName = event.target.value;
-    }
+  handleNameChange(event) {
+    this.newAccountName = event.target.value;
+  }
 
-    handleCreate() 
-    {
-        createAccount({ name: this.newAccountName })
-            .then(() => {
-                this.newAccountName = '';
-                this.loadAccounts();
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
+  handleCreate() {
+    createAccount({ name: this.newAccountName })
+      .then(() => {
+        this.newAccountName = "";
+        this.loadAccounts();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
-    handleDelete(event) {
-        const accountId = event.target.dataset.accountId;
-        deleteAccount({ accountId })
-            .then(() => {
-                this.loadAccounts();
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
+  handleDelete(event) {
+    const accountId = event.target.dataset.accountId;
+    deleteAccount({ accountId })
+      .then(() => {
+        this.loadAccounts();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
